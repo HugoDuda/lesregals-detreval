b11e1e4784d8180d5559196d54c3167151f23c86
Hugo_DUDA <hugo.duda@protonmail.com>
2025-02-09 17:55:09 +0100

    :rocket: Fix part of responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./**/*.html"],
   theme: {
     extend: {},
+        screens: {
+      'sm': '640px',
+      'md': '900px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
